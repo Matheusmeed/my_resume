699f2c6efdd9295258014cdbd1cd3f6b7f15986b
Matheus <mms.medeiros12@gmail.com>
2023-12-20 02:40:43 -0300

    feat: adding test section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import AboutMe from './sections/AboutMe';
 import Profile from './sections/Profile';
+import Test from './sections/TestSection';
 
 const App = () => {
   return (
     <div>
       <Profile />
       <AboutMe />
+      <Test />
     </div>
   );
 };
---
+++ src/sections/TestSection/index.tsx
@@ -0,0 +1,9 @@
+import { SectionScreenWrapper } from '../../shared/styles/styles';
+
+const Test = () => {
+  return (
+    <SectionScreenWrapper backgroundColor='pink'>Profile</SectionScreenWrapper>
+  );
+};
+
+export default Test;
